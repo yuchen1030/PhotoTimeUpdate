ec386489b4cf8cbc8abd93466a21cfe996080f0c
yuchen1030 <yuchen1030@163.com>
2022-11-20 15:02:16 +0800

    添加项目文件。

---
+++ PhotoTimeUpdate/MainWindow.xaml.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing.Imaging;
+//using Ookii.Dialogs.Wpf;
+using PhotoTimeUpdate.Services;
+using System.Windows.Threading;
+using System.Threading;
+
+namespace PhotoTimeUpdate
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TextBox_StartT.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            TextBox_EndT.Text = DateTime.Now.AddSeconds(1).ToString("yyyy-MM-dd HH:mm:ss");
+            TextBox_Gap.Text = "-1";
+            Loaded += new RoutedEventHandler(Window1_Loaded);
+        }
+
+
+        private void Button_Compute_Click(object sender, RoutedEventArgs e)
+        {
+            string computePath = TextBox_Compute.Text;
+            if (!string.IsNullOrEmpty(computePath))
+            {
+                TextBox_StartT.Text = "";
+                TextBox_EndT.Text = "";
+                TextBox_Gap.Text = "";
+                if (!Directory.Exists(computePath))
+                {
+                    MessageBox.Show("您输入的文件夹【" + computePath + "】不存在"  );
+                    return;
+                }
+                else
+                {
+                    var files = Directory.GetFiles(computePath).Take(2).ToList();
+                    if (files.Count == 2)
+                    {
+                        try
+                        {
+                            TextBox_StartT.Text = ExifFix.GetTakePicDate(files[0]);
+                            TextBox_EndT.Text = ExifFix.GetTakePicDate(files[1]);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("请确保您的文件是图片格式:" + ex.Message);
+                            return;
+                        }
+                    }
+
+                }
+            }
+            try
+            {
+                TextBox_Gap.Text = (Convert.ToDateTime(TextBox_StartT.Text) - Convert.ToDateTime(TextBox_EndT.Text)).TotalSeconds.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("请确保时间格式正确:" + ex.Message);
+            }
+
+
+        }
+
+
+
+        int cnt = 3;
+        private void Button_UpdateTime_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextBox_PicFolder.Text))
+            {
+                TextBox_PicFolder.Text = @"D:\BaiduYunDownload\2022.10.2婚照原片";
+            }
+            Label_UpdateResult.Text = "尚未有执行结果。";
+            UpdatePictureTime();
+            cnt++;
+        }
+
+        private void UpdatePictureTime()
+        {
+            string folder = TextBox_PicFolder.Text;
+            if (!string.IsNullOrWhiteSpace(folder))
+            {
+                if (!Directory.Exists(folder))
+                {
+                    MessageBox.Show("您输入的文件夹【" + folder + "】不存在，请确认");
+                    return;
+                }
+                Int64 secs = 0;
+                try
+                {
+                    secs = Convert.ToInt64(TextBox_Gap.Text.Trim());
+                }
+                catch
+                {
+
+                }
+                if (secs == 0)
+                {
+                    MessageBox.Show("时间差值为0，无需修改，请确保您输入了正确的差值秒数");
+                    return;
+                }
+                var files = Directory.GetFiles(folder, "*.*").Count();
+
+                if (System.Windows.MessageBox.Show("您确认要对该目录下的【" + files + "】张照片的拍摄时间加上【" + secs + "】秒嘛？", "提示：", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+                {
+                    timer.Start();
+                    ThreadPool.QueueUserWorkItem(_ => UpdatePhotoInfo(files, folder, secs));
+                }
+            }
+        }
+
+
+        void UpdatePhotoInfo(int files, string folder, Int64 secs)
+        {
+            UpdateUiText(Label_UpdateResult, "执行中，请稍候......");
+            //Label_UpdateResult.Text = "执行中，请稍候......";
+            DateTime st = DateTime.Now;
+            List<String> returnMsg = ExifFix.FixExifDateTime(folder, secs);
+            DateTime et = DateTime.Now;
+            var spanStr = (et - st).TotalMinutes.ToString("f2");
+            UpdateUiText(Label_UpdateResult, "耗时" + spanStr + "分，执行完成，请参见下方文本框的结果。");
+            //Label_UpdateResult.Text = "耗时" + spanStr + "分，执行完成，请参见下方文本框的结果。";
+            string msg = files + "张原始图片的拍摄时间保存于【" + returnMsg[0] + "】路径下，已完成了【" + returnMsg[1] + "】张图片的时间更新，总耗时" + spanStr + "分(" + st.ToString("HH:mm:ss") + "~" + et.ToString("HH:mm:ss") + ")";
+            //TextBox_UpdateResult.Text = msg + Environment.NewLine + Environment.NewLine + returnMsg[2];
+            UpdateUiTextBox(TextBox_UpdateResult, msg + Environment.NewLine + Environment.NewLine + returnMsg[2]);
+            timer.Stop();
+            timerCnt = 0;
+        }
+
+
+
+
+        private delegate void UpdateUiTextDelegate(TextBlock control, string text);
+        private void UpdateUiText(TextBlock control, string text)
+        {
+            if (!Dispatcher.CheckAccess())
+            {
+                Dispatcher.Invoke(DispatcherPriority.Send, new UpdateUiTextDelegate(UpdateUiText), control, text);
+                return;
+            }
+            control.Text = text;
+        }
+
+
+        private delegate void UpdateUiTextBoxDelegate(TextBox control, string text);
+        private void UpdateUiTextBox(TextBox control, string text)
+        {
+            if (!Dispatcher.CheckAccess())
+            {
+                Dispatcher.Invoke(DispatcherPriority.Send, new UpdateUiTextBoxDelegate(UpdateUiTextBox), control, text);
+                return;
+            }
+            control.Text = text;
+        }
+
+
+        private DispatcherTimer timer;
+
+
+
+        void Window1_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer1_Tick;
+
+        }
+        int timerCnt = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timerCnt++;
+            UpdateUiText(Label_UpdateResult, "执行中，请稍候......已执行 " + timerCnt + " 秒");
+
+        }
+
+
+
+    }
+}
---
+++ PhotoTimeUpdate/Services/ExifFix.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PhotoTimeUpdate.Services
+{
+    public class ExifFix
+    {
+        public static List<String> FixExifDateTime(string photoPath, Int64 secs)
+        {
+            List<string> oriPicInfos = new List<string>();
+            List<string> tarPicInfos = new List<string>();
+            List<string> tarPicComputes = new List<string>();
+            string[] files = new string[0];
+            if (File.Exists(photoPath))
+            {
+                files = new string[] { photoPath };
+            }
+            else
+            {
+                files = Directory.GetFiles(photoPath); //获取所有照片路径
+            }
+            photoPath = photoPath.Trim("/\\".ToCharArray());
+            string subFolder = photoPath.Substring(photoPath.LastIndexOf("/") + photoPath.LastIndexOf("\\") + 2);
+
+            int i = 0;
+            IFormatProvider culture = new CultureInfo("zh-CN", true);
+            foreach (var file in files)
+            {
+                i++;
+                //替代System.Drawing.Image.FromFile(file);
+                System.Drawing.Image image = GetImage(file);
+
+                //获取Exif的DateTimeOriginal属性（36867）
+                PropertyItem pi = image.GetPropertyItem(36867); //(int)ExifTags.DateTimeOriginal
+
+                //转为时间文本
+                string oldTime = Encoding.ASCII.GetString(pi.Value);
+                oldTime = oldTime.Replace("\0", "");
+
+                Debug.WriteLine(file + " " + oldTime);
+                //时间文本格式化为DateTime
+                DateTime time = DateTime.ParseExact(oldTime, "yyyy:MM:dd HH:mm:ss", culture);
+                oriPicInfos.Add(i + "," + file + "," + time.ToString("yyyy-MM-dd HH:mm:ss"));
+
+                //得到正确的时间
+                DateTime newTime = time.AddSeconds(secs);
+                tarPicComputes.Add(newTime.ToString("yyyy-MM-dd HH:mm"));
+                tarPicInfos.Add(i + "," + file + "," + newTime.ToString("yyyy-MM-dd HH:mm:ss"));
+                //转换为EXIF存储的时间格式
+                string newTimeString = newTime.ToString("yyyy:MM:dd HH:mm:ss");
+                pi.Value = Encoding.ASCII.GetBytes(newTimeString + "\0");
+
+                //修改DateTimeOriginal属性和其他的时间属性
+                image.SetPropertyItem(pi);
+                pi.Id = 306; // (int)ExifTags.DateTime; //306
+                image.SetPropertyItem(pi);
+                pi.Id = 36868; //(int)ExifTags.DateTimeDigitized; //36868
+                image.SetPropertyItem(pi);
+
+                //存回文件
+                image.Save(file);
+
+                image.Dispose();
+            }
+
+            string path0 = Directory.GetCurrentDirectory();
+            String res = string.Join(Environment.NewLine, oriPicInfos);
+            string oriWhole = System.IO.Path.Combine(path0, subFolder + "_" + DateTime.Now.ToString("yyyyMMdd-HHmmss") + "_原始图片拍摄时间.csv");
+            System.IO.File.WriteAllText(oriWhole, res);
+
+            res = string.Join(Environment.NewLine, tarPicInfos);
+            string whole = System.IO.Path.Combine(path0, subFolder + "_" + DateTime.Now.ToString("yyyyMMdd-HHmmss") + "_修改后图片拍摄时间.csv");
+            System.IO.File.WriteAllText(whole, res);
+
+            var result = tarPicComputes.GroupBy(a => a).Select(a => new { key = a.Key, val = a.Count() }).ToList().Select(a => a.key + "：" + a.val).ToList();
+
+            res = string.Join(Environment.NewLine, result);
+            whole = System.IO.Path.Combine(path0, subFolder + "_" + DateTime.Now.ToString("yyyyMMdd-HHmmss") + "_每分钟拍摄图片张数统计.txt");
+            System.IO.File.WriteAllText(whole, res);
+
+            List<String> returnMsg = new List<string>();
+            returnMsg.Add(path0);
+            returnMsg.Add(tarPicComputes.Count().ToString());
+            returnMsg.Add("每分钟拍摄图片张数统计如下：" + Environment.NewLine + res);
+            return returnMsg;
+        }
+
+        public static DateTime GetDateTimeOriginal(System.Drawing.Image image)
+        {
+            PropertyItem pi = image.GetPropertyItem(36867); //(int)ExifTags.DateTimeOriginal
+            string oldTime = Encoding.ASCII.GetString(pi.Value);
+            oldTime = oldTime.Replace("\0", "");
+
+            //时间文本格式化为DateTime
+            IFormatProvider culture = new CultureInfo("zh-CN", true);
+            DateTime time = DateTime.ParseExact(oldTime, "yyyy:MM:dd HH:mm:ss", culture);
+            return time;
+        }
+
+        public static System.Drawing.Image GetImage(string filePath)
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    return null;
+                }
+
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    byte[] bytes = new byte[fileStream.Length];
+                    fileStream.Read(bytes, 0, bytes.Length);
+
+                    MemoryStream memoryStream = new MemoryStream(bytes);
+                    if (memoryStream != null)
+                    {
+                        return System.Drawing.Image.FromStream(memoryStream);
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+
+            return null;
+        }
+
+
+
+        public static string GetTakePicDate(string fileName)
+        {
+            Encoding ascii = Encoding.ASCII;
+            string picDate;
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            Image image = Image.FromStream(stream, true, false);
+            foreach (PropertyItem p in image.PropertyItems)
+            {
+                //获取拍摄日期时间
+                if (p.Id == 0x9003) // 0x0132 最后更新时间
+                {
+                    stream.Close();
+                    picDate = ascii.GetString(p.Value);
+                    picDate = picDate.Replace("\0", "");
+                    IFormatProvider culture = new CultureInfo("zh-CN", true);
+                    DateTime time = DateTime.ParseExact(picDate, "yyyy:MM:dd HH:mm:ss", culture);
+                    if ((!"".Equals(picDate)) && picDate.Length >= 10)
+                    {
+                        return time.ToString("yyyy-MM-dd HH:mm:ss");
+                    }
+                }
+            }
+            stream.Close();
+            return "";
+        }
+
+
+
+
+    }
+}
